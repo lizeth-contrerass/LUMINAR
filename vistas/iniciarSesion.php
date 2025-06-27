dfc09ceadf01178e93eae6268bb12b6e73e7a59d
AlexanderMaldonadoHernandez <maldonado.h.alexander0@gmail.com>
2025-06-26 22:54:34 -0600

    iniciarSesion terminado

---
+++ vistas/iniciarSesion.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Inicia sesión</title>
+        <link rel="stylesheet" href="../estilo/registro-inicio.css" type="text/css">
+    </head>
+    
+    <body>
+
+        <header>
+            <img src="../imagenes/logo-sin-fondo.png" alt="Logo de Luminar">
+        </header>
+
+        <main>
+        
+            <h1>Inicia de sesión</h1>
+
+            <form method="POST" action="./loginCandidato.php">
+                <p>Correo electrónico</p>  
+                <input type="text" name="usuario" required>
+            
+                <p>Contraseña</p>
+                <input type="password" name="contrasena" required>
+                
+                <button type="submit">Iniciar sesión</button>
+            </form>
+
+            <?php
+            if(isset($errorLogin)){
+                echo $errorLogin;
+            }
+            ?>
+
+        </main>
+
+        <footer>
+            <p>¿No tienes cuenta?</p>
+            <p>Si buscas empleo regístrate <a href="./registroCandidato.php">aquí</a>.</p>
+            <p>Si buscas talento regístrate <a href="./registroEmpresa.php">aquí</a>.</p>
+        </footer>
+
+    </body>
+
+</html>
